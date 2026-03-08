35fae402b5eedcf87aac4f80cad41d824252e94c
windyhillville <benjamindavidson@ufl.edu>
2026-03-08 05:22:27 -0400

    feat(lint): comment out unused mock data

--- frontend/src/app/(employee)/schedule/index.tsx
+++ frontend/src/app/(employee)/schedule/index.tsx
@@ -1,15 +1,15 @@
-import { DayItem, Header, Screen } from '@/ui';
+import { Header, Screen } from '@/ui';
 // import { Text } from 'react-native';
 
-const fullWeek: DayItem[] = [
-  { key: 'sun', label: 'Su', timeRange: '7 AM - 3 PM' },
-  { key: 'mon', label: 'Mo', timeRange: '9 AM - 5 PM' },
-  { key: 'tue', label: 'Tu', timeRange: '9 AM - 5 PM' },
-  { key: 'wed', label: 'We', timeRange: '9 AM - 5 PM' },
-  { key: 'thur', label: 'Th', timeRange: '9 AM - 5 PM' },
-  { key: 'fri', label: 'Fr', timeRange: '6 PM - 2 AM' },
-  { key: 'sat', label: 'Sa', timeRange: '12 PM - 8 PM' },
-];
+// const fullWeek: DayItem[] = [
+//   { key: 'sun', label: 'Su', timeRange: '7 AM - 3 PM' },
+//   { key: 'mon', label: 'Mo', timeRange: '9 AM - 5 PM' },
+//   { key: 'tue', label: 'Tu', timeRange: '9 AM - 5 PM' },
+//   { key: 'wed', label: 'We', timeRange: '9 AM - 5 PM' },
+//   { key: 'thur', label: 'Th', timeRange: '9 AM - 5 PM' },
+//   { key: 'fri', label: 'Fr', timeRange: '6 PM - 2 AM' },
+//   { key: 'sat', label: 'Sa', timeRange: '12 PM - 8 PM' },
+// ];
 
 // const halfWeek: DayItem[] = [
 //   { key: 'sun', label: 'Su', timeRange: '7 AM - 3 PM' },
